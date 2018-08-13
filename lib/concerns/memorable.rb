0dfe8245fd2d66605fe20aa8820e9c13f232bf90
Juan Tenezaca <tenj34@gmail.com>
2018-08-13 23:04:53 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,16 @@
 module Memorable
-  def reset_all
-    all.clear
+  module ClassMethods
+  def count
+    self.all.count
   end
 
-  def count
-    all.count
+  def reset_all
+    self.all.clear
   end
+end
+module InstanceMethods
+  def initialize
+    self.class.all << self
+  end
+end
 end
