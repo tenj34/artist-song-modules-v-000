7e1aa2f840dcb3f380a4320471775589c4171e82
Juan Tenezaca <tenj34@gmail.com>
2018-08-13 23:01:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,11 +11,6 @@ class Song
   def initialize
     @@songs << self
   end
-=begin
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-=end
 
   def artist=(artist)
     @artist = artist
